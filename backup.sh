606bb72ba89243b6116451dba33b503771ab1f3b
Joseph Ziegler <zieglerj@zigsphere.com>
2017-03-04 20:05:19 -0800

    Edit bash file

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-date_file=date +"%d.%m.%y"
-files=($(find /home/pi/flash/cam-pics -mtime -7)) #Path that contains the images
-tar cvfz $date_file.tar.gz "${files[@]}"
+date_file=$(date +%d.%m.%y)
+cd /home/pi/flash/cam-pics
+files=($(find . -mtime +1 -type f)) #Path that contains the images
+tar cvfz $date_file.tar.gz "${files[@]}" #Compress files
 /home/pi/gdrive-linux-rpi upload $date_file.tar.gz -p <insert your Google Drive directory id here>
+rm $date_file.tar.gz #Remove compressed file after uploading to Google Drive
+rm "${files[@]}"     #Remove all of the pictures that were compressed and uploaded to Google Drive
