cdd71d4d3245653ed267d6652a4ccca55effa0e5
Joseph Ziegler <joseph@zigsphere.com>
2017-03-04 22:11:50 -0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 date_file=$(date +%d.%m.%y)
-cd /home/pi/flash/cam-pics
-files=($(find . -mtime +1 -type f)) #Path that contains the images
+cd /home/pi/flash/cam-pics #Path that contains the images
+files=($(find . -mtime +1 -type f)) #Find files that are older than 1 day
 tar cvfz $date_file.tar.gz "${files[@]}" #Compress files
 /home/pi/gdrive-linux-rpi upload $date_file.tar.gz -p <insert your Google Drive directory id here>
 rm $date_file.tar.gz #Remove compressed file after uploading to Google Drive
